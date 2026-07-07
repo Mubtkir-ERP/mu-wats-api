c5c736bf5d3f7c21bcf3ffb663a549228dc059a4
Mubtkir <149900840+Mubtkir@users.noreply.github.com>
2026-07-08 00:13:43 +0300

    Update hooks.py

--- frappe_whatsapp/hooks.py
+++ frappe_whatsapp/hooks.py
@@ -111,6 +111,10 @@ doctype_js = {"Whatsapp Instance": "public/js/whatsapp_instance.js"}
 #   }
 # }
 
+fixtures = [
+    {"dt": "Number Card", "filters": [["document_type", "=", "WhatsApp Message"]]}
+]
+--------------------
 # Scheduled Tasks
 # ---------------
 
