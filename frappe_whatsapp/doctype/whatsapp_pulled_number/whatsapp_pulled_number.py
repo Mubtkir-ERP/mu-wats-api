288501cf24ee90d82be08d77307113438b049e22
Mubtkir <almubtkir@gmail.com>
2026-09-11 04:40:49 +0300

    delete all files

--- frappe_whatsapp/doctype/whatsapp_pulled_number/whatsapp_pulled_number.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2026, MUBTKIR and contributors
-# For license information, please see license.txt
-
-from frappe.model.document import Document
-
-
-class WhatsAppPulledNumber(Document):
-	pass
